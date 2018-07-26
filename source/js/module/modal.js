f648577d204649a015703cc0dc56c48b4b4cbcdc
katyamatya21 <katyamatya21@gmail.com>
2018-07-26 21:53:02 +0300

    Немного наводит порядок

--- source/js/module/modal.js
+++ source/js/module/modal.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
 
   /**
    * Modal controller
@@ -13,7 +13,7 @@
   /**
    * Initialization, button click handler to open modals
    */
-  ModalsController.prototype.init = function() {
+  ModalsController.prototype.init = function () {
     var buttons = document.querySelectorAll('[data-type]');
     var self = this;
     buttons.forEach(function (item) {
@@ -24,7 +24,7 @@
   /**
    *  Opens modal, button click handler to close modal
    */
-  ModalsController.prototype.onModalButtonClick = function(event) {
+  ModalsController.prototype.onModalButtonClick = function (event) {
     var typeModal = event.currentTarget.dataset.modal;
     this._activeModal = this.findModal(typeModal);
     this._activeModal.classList.add('modal--opened');
@@ -36,7 +36,7 @@
   /**
    * Closes modal, removes button handler closing modal
    */
-  ModalsController.prototype.onModalCloseClick = function() {
+  ModalsController.prototype.onModalCloseClick = function () {
     this._activeModal.querySelector('.js-close').removeEventListener('click', this._closeListener);
     this._activeModal.classList.remove('modal--opened');
     document.querySelector('.wrap').classList.remove('wrap--blur');
@@ -49,7 +49,7 @@
    * @param typeModal
    * @returns {Element}
    */
-  ModalsController.prototype.findModal = function(typeModal) {
+  ModalsController.prototype.findModal = function (typeModal) {
     return document.querySelector('[data-modal-type=' + typeModal + ']');
   };
 
